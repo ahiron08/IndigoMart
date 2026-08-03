e13f3c99241ca20e344ce1128c634572a0f05071
ahiron08 <ahironsharma08@gmail.com>
2026-08-04 00:22:24 +0530

    wow3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,30 +4,30 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+
+    build: {
+      sourcemap: true,
+    },
+
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:5000',
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
-
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd(), '');
-
-//   return {
-//     plugins: [react(), tailwindcss()],
-//     resolve: {
-//       alias: {
-//         '@': fileURLToPath(new URL('./src', import.meta.url)),
-//       },
-//     },
-//     server: {
-//       port: 5173,
-//       proxy: {
-//         '/api': {
-//           target: env.VITE_API_URL || 'http://localhost:5000',
-//           changeOrigin: true,
-//         },
-//       },
-//     },
-//   };
-// });
